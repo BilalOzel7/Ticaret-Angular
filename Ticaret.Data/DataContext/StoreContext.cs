efeb284c593b5a52697a740f729e97b7a48e7d72
BilalOzel7 <mbozel07@gmail.com>
2021-10-20 01:11:12 +0300

    Add project files.

---
+++ Ticaret.Data/DataContext/StoreContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticaret.Data.DbModels;
+
+namespace Ticaret.Data.DataContext
+{
+   public class StoreContext:DbContext
+    {
+        public StoreContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
